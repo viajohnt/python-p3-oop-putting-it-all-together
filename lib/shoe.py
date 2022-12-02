aa81c9465664be509c9757add1d00bec7a599e36
Ben Botsford <ben.botsford@flatironschool.com>
2022-12-02 09:20:48 -0600

    add properties to tests

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self, brand):
+    def __init__(self, brand, size=0):
         self.brand = brand
-
+        self._size = size
 
     def cobble(self):
         self.condition = "New"
         print('Your shoe is as good as new!')
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, value):
+        
+        if type(value) == int:
+            self._size = value
+
+        print("size must be an integer")
+
+    size = property(get_size, set_size)
